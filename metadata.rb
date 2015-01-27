1a0052080a49a4093551cec73c90744eb81c2f7b
Adam Cooper <adam.cooper@nottingham.ac.uk>
2015-01-27 14:27:58 +0000

    Attempts to fix issues with berkshelf not behaving.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
+name             "tomcat-solr"
 maintainer       "Adam Cooper"
 maintainer_email "adam@networkpie.co.uk"
 license          "Apache 2.0"
 description      "Installs/Configures solr"
-
-version          "0.1.1"
+version          "0.1.2"
