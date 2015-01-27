54f5b8063baeea965b2a3ce04b776c4b6b9f338f
Adam Cooper <adam.cooper@nottingham.ac.uk>
2015-01-27 14:34:26 +0000

    More attempts to fix this berkshelf thing.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,15 @@
 name             "tomcat-solr"
 maintainer       "Adam Cooper"
 maintainer_email "adam@networkpie.co.uk"
-license          "Apache 2.0"
+license          "BSD"
 description      "Installs/Configures solr"
 version          "0.1.2"
+
+recipe "solr", "Install and setup a solr instance"
+
+depends "apt", ">= 0.0.0"
+depends "java", ">= 0.0.0"
+
+%w{ ubuntu debian }.each do |os|
+  supports os
+end
